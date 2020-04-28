7085ea221c676113e79b1ce0e3341762044556aa
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2020-04-29 01:56:55 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get install git
+sudo apt-get install vim-gui-common 
+sudo apt-get install vim-runtime
+sudo apt-get install bluetooth bluez-utils blueman
+sudo apt-get install ssh
+sudo apt-get install openssh-server
+sudo sudo apt-get install vsftpd
+FILE="./bashrc"
+sudo echo "export VISUAL=\"vim\"" > $FILE
+sudo echo "export EDITOR=\"$VISUAL\""> $FILE
+sudo cp ~/20200428success/vimrc ~/.vimrc
+sudo update-rc.d ssh defaults
