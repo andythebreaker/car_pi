2156267a4c7c4107278c3aecb901f159fd310a55
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2020-07-16 23:18:53 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,28 @@ console.log("start index.js");//comment
 $(document).ready(function(){//YOUR CODE HERE
   'use strict';
   console.log("document ready");
+  
+  console.log("load header.js");
+  function readTextFile(file)
+  {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+                return allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+var theDiv = document.getElementById("headerJS");
+var content = document.createTextNode(readTextFile("https://raw.githubusercontent.com/andythebreaker/car_pi/gh-pages/header.html"));
+theDiv.appendChild(content);
+console.log("end header.js");
 });
