59da15f49fbc96f80b70ef9a026dd0b58a1a345d
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2020-07-16 22:55:01 +0800

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,4 +1,20 @@
 console.log("load header.js");
-var fs = require('fs'); 
-var data = fs.readFileSync('../header.html', 'utf8');
-console.log(data);
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+readTextFile("../header.html");
+console.log("end header.js");
