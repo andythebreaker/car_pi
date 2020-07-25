51876228865bc697e2e475dfc753bf9fe95a69f3
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2020-07-25 13:49:41 +0800

    Add files via upload

---
+++ CarAudioOBD/js/header.js
@@ -0,0 +1,20 @@
+console.log("load header.js");
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+readTextFile("https://raw.githubusercontent.com/andythebreaker/car_pi/gh-pages/header.html");
+console.log("end header.js");
